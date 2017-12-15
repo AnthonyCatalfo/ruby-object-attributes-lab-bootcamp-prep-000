77d0a2f93467b5468de2fab4ddb3b724ebd14f28
Anthony Catalfo <anthonycatalfo@gmail.com>
2017-12-15 23:31:28 +0000

    automatically backed up by learn

---
+++ lib/person.rb
@@ -0,0 +1,20 @@
+class Dog
+
+
+  def name=(name)
+    @name=name
+  end
+
+  def name
+    "#{@name}".strip
+  end
+  def job=(job)
+    @job=job
+  end
+
+  def job
+    "#{@job}".strip
+  end
+
+
+end
